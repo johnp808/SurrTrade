604fabda77a1a132eadfef9146e335e04b3f8419
Me <johnpuliatch@gmail.com>
2024-11-24 23:28:13 -1000

    generated header and footer components and start on header functionality

--- ngSurrTrade/src/app/app.module.ts
+++ ngSurrTrade/src/app/app.module.ts
@@ -11,9 +11,18 @@ import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, ProfileComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   bootstrap: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule, FormsModule],
   providers: [provideHttpClient(withInterceptorsFromDi())],
--- ngSurrTrade/src/app/components/login/login.component.ts
+++ ngSurrTrade/src/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   loginData = {
